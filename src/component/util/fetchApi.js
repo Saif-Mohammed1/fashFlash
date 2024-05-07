8a3a23cd314a83684d3ed97b9788ed23d98b8b1e
saif_mohammed1 <sm086189@gmail.com>
2024-05-07 15:29:00 +0300

    upload

--- src/component/util/fetchApi.js
+++ src/component/util/fetchApi.js
@@ -3,7 +3,11 @@ import { signOut } from "next-auth/react";
 const fetchApi = async (url, options = {}) => {
   try {
     // Make a fetch request
-    const response = await fetch(process.env.API_ENDPOINT + url, options);
+    const response = await fetch(
+      // process.env.NEXT_PUBLIC_API_ENDPOINT
+      "/api" + url,
+      options
+    );
 
     if (!response.ok) {
       const err = await response.json();
