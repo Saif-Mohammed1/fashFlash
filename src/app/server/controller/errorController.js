795f4ec22be21ed31e572008c2202a40abdf3503
Saif Mohammed <74511950+Saif-Mohammed1@users.noreply.github.com>
2024-05-08 23:05:40 +0300

    Update errorController.js

--- src/app/server/controller/errorController.js
+++ src/app/server/controller/errorController.js
@@ -73,7 +73,7 @@ const sendErrorProd = (err, req) => {
       {
         status: "error",
         message:
-          err.message ||
+          // err.message ||
           "Something went very wrong!. If the error persists, please let us know. ",
       },
       { status: 500 }
