8a68abe010da84866d8f833e1333e1b8b0ac3ba7
saif_mohammed1 <sm086189@gmail.com>
2024-05-11 19:29:43 +0300

    update error code

--- src/app/server/controller/reviewsController.js
+++ src/app/server/controller/reviewsController.js
@@ -1,6 +1,5 @@
 import AppError from "@/component/util/appError";
-import Order from "../models/order.model";
-import Address from "../models/address.model";
+import Order from "../models/order.model ";
 
 export const createReviews = async (req, model) => {
   let doc;
@@ -11,12 +10,12 @@ export const createReviews = async (req, model) => {
       throw new AppError("Rating is required", 400);
     }
     if (!reviewText) {
-      throw new AppError("Review text is required", 400);
+      throw new AppError("reviewText is required", 400);
     }
 
     if (rating < 1) {
       throw new AppError(
-        "Rating cannot be less than 1. Current rating is: " + rating,
+        "Rating cannot be less than 1. Current rate is: " + rating,
         400
       );
     }
@@ -27,30 +26,30 @@ export const createReviews = async (req, model) => {
       reviewText,
     });
     return {
+      // data: doc,
       data: doc,
       statusCode: 201,
     };
   } catch (error) {
+    //console.log("error createReviews", error);
     if (doc) {
-      await model.findOneAndDelete({
-        product: req.id,
-        user: req.user._id,
-      });
+      await model.findOneAndDelete(
+        { product: req.id, user: req.user._id } // Condition to find the document
+      );
     }
     throw error;
   }
 };
-
 export const deleteReview = async (req, model) => {
   try {
-    const doc = await model.findOneAndDelete({
-      product: req.id,
-      user: req.user._id,
-    });
+    const doc = await model.findOneAndDelete(
+      { product: req.id, user: req.user._id } // Condition to find the document
+    );
     if (!doc) {
-      throw new AppError("No review found for deletion", 404);
+      throw new AppError("No documents found with", 404);
     }
     return {
+      // data: doc,
       data: null,
       statusCode: 200,
     };
@@ -63,6 +62,7 @@ export const checkReview = async (req, model) => {
   try {
     const order = await Order.findOne({
       user: req.user._id,
+      // product: req.id,
     });
 
     if (!order) {
@@ -73,7 +73,7 @@ export const checkReview = async (req, model) => {
     }
     if (order.status !== "completed") {
       throw new AppError(
-        "Thank you for your interest in leaving a review! Please note that you need to wait until your order is completed before you can leave a review. Once your order is completed, you'll be able to share your feedback with us. We appreciate your patience and understanding",
+        "Thank you for your interest in leaving a review! Please note that you need to wait until  your order complete before you can leave a review. Once your order is completed, you'll be able to share your feedback with us. We appreciate your patience and understanding",
         404
       );
     }
@@ -93,6 +93,6 @@ export const checkReview = async (req, model) => {
       statusCode: 200,
     };
   } catch (error) {
-    throw error;
+    throw error; // Rethrow the error to be caught by the caller
   }
 };
